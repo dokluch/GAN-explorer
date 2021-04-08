f413b07f5b1b5dcee75182a91f2db607ef472a1d
Nik Ska <dokluch@gmail.com>
2021-04-08 18:41:10 +0300

    G -> Gs

--- gan_explorer.py
+++ gan_explorer.py
@@ -53,7 +53,7 @@ def make_img_from_seed(Gs, seed_in = 0):
     z1 = torch.randn([1, Gs.z_dim]).cuda() 
 
     w = Gs.mapping(z1, None, truncation_psi=0.7, truncation_cutoff=8)
-    img = G.synthesis(w, noise_mode='const', force_fp32=True)
+    img = Gs.synthesis(w, noise_mode='const', force_fp32=True)
 
     img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
     img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB')
