e39a23a493b98a0c078c060262ed877aa0484ee0
Nik Ska <dokluch@gmail.com>
2021-04-09 21:51:57 +0300

    added ISR to the models list

--- download.py
+++ download.py
@@ -3,13 +3,29 @@ import tarfile
 import argparse
 
 SOURCES = {
-    'art-faces-256': 'https://www.dropbox.com/s/8shgau6vvj8mfcw/256-art-faces.tar',
-    'art-landscapes-art-256': 'https://www.dropbox.com/s/wc2jr1c0kzgxgzf/256-art-landscapes.tar',
-    'mountains-256': 'https://www.dropbox.com/s/7e3y2xkn08mthct/256-mountains.tar',
+    'stylegan2': {
+        'art-faces-256': 'https://www.dropbox.com/s/8shgau6vvj8mfcw/256-art-faces.tar',
+        'art-landscapes-art-256': 'https://www.dropbox.com/s/wc2jr1c0kzgxgzf/256-art-landscapes.tar',
+        'mountains-256': 'https://www.dropbox.com/s/7e3y2xkn08mthct/256-mountains.tar',
+    },
+    'ISR': {
+        'ISR': 'https://www.dropbox.com/s/o6oiq3nol69fwb0/rrdn-C4-D3-G32-G032-T10-x4.tar',
+    }
 }
 
+SUBFOLDERS = {}
+LINKS = {}
+MODEL_NAMES = []
 
-def download(source, destination):
+for k in SOURCES.keys():
+    for kk in SOURCES[k].keys():
+        MODEL_NAMES.append(kk)
+        LINKS[kk] = SOURCES[k][kk]
+        SUBFOLDERS[kk] = k
+
+
+def download(source, destination, subfolder):
+    destination = os.path.join(destination, subfolder)
     tmp_tar = os.path.join(destination, '.tmp.tar')
     # urllib has troubles with dropbox
     os.system(f'wget {source} -O {tmp_tar}')
@@ -22,7 +38,7 @@ def download(source, destination):
 def main():
     parser = argparse.ArgumentParser(description='Pretrained models loader')
     parser.add_argument('--models', nargs='+', type=str,
-                        choices=list(SOURCES.keys()) + ['all'], default=['all'])
+                        choices=MODEL_NAMES + ['all'], default=['all'])
     parser.add_argument('--out', type=str, help='root out dir')
 
     args = parser.parse_args()
@@ -32,12 +48,12 @@ def main():
 
     models = args.models
     if 'all' in models:
-        models = list(SOURCES.keys())
+        models = MODEL_NAMES
 
     for model in set(models):
-        source = SOURCES[model]
+        source = LINKS[model]
         print(f'downloading {model}\nfrom {source}')
-        download(source, args.out)
+        download(source, args.out, SUBFOLDERS[model])
 
 
 if __name__ == '__main__':
